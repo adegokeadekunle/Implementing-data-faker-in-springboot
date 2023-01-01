d085f188cff0063c242945e9c979223cc3084a36
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2023-01-01 23:49:08 +0100

    first 2023 commit

---
+++ src/main/java/com/adekunle/datafaker/DataFakerApplication.java
@@ -0,0 +1,13 @@
+package com.adekunle.datafaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adekunle/datafaker/DataFakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adekunle.datafaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
